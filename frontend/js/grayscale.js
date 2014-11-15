a96dc276e808064daece138c11ba147c75325309
Christie Daniels <chris@GAIUS.local>
2014-11-15 15:06:24 -0500

    added logged in page

--- frontend/js/grayscale.js
+++ frontend/js/grayscale.js
@@ -160,19 +160,4 @@ function init() {
         }]
     };
 
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
-
-    // Create the Google Map using out element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
-
-    // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-    var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
-    var beachMarker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        icon: image
-    });
 }
